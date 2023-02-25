0082e5cb0ff11f701dff235a09c804be6c131f7d
Zynh0722 <Zynh0722@gmail.com>
2023-02-25 13:35:00 -0800

    add shuffboard stuff

--- 2023_ChargedUp/src/main/java/frc/robot/subsystems/Arm.java
+++ 2023_ChargedUp/src/main/java/frc/robot/subsystems/Arm.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.WPI_CANCoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -13,6 +14,12 @@ import cowlib.DualProfiledPIDSubsystem;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.LayoutType;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.ArmConstants.PID;
@@ -21,12 +28,16 @@ import static frc.robot.Constants.ArmConstants.*;
 import java.util.function.DoubleSupplier;
 
 public class Arm extends DualProfiledPIDSubsystem {
+  ShuffleboardTab shuffTab = Shuffleboard.getTab("Arm");
 
   private CANSparkMax lowerArmMotor = new CANSparkMax(ArmConstants.lowerArmMotorID, MotorType.kBrushless);
   private CANSparkMax upperArmMotor = new CANSparkMax(ArmConstants.upperArmMotorID, MotorType.kBrushless);
 
-  private CANCoder lowerArmEncoder = new CANCoder(ArmConstants.lowerArmEncoderID);
-  private CANCoder upperArmEncoder = new CANCoder(ArmConstants.upperArmEncoderID);
+  private WPI_CANCoder lowerArmEncoder = new WPI_CANCoder(ArmConstants.lowerArmEncoderID);
+  private WPI_CANCoder upperArmEncoder = new WPI_CANCoder(ArmConstants.upperArmEncoderID);
+
+  private NetworkTableEntry lowerArmEncoderEntry;
+  private NetworkTableEntry upperArmEncoderEntry;
 
   private double pose = 0;
 
@@ -54,6 +65,24 @@ public class Arm extends DualProfiledPIDSubsystem {
 
     lowerArmMotor.setIdleMode(IdleMode.kBrake);
     upperArmMotor.setIdleMode(IdleMode.kBrake);
+
+    // PID Controllers in the Arm Tab
+    ShuffleboardLayout pidContainer = shuffTab
+      .getLayout("PID Controllers", BuiltInLayouts.kList)
+      .withSize(2, 4)
+      .withPosition(0, 0);
+
+    pidContainer.add("Lower PID Controller", m_controllerA);
+    pidContainer.add("Upper PID Controller", m_controllerB);
+
+    // Encoders in the Arm Tab
+    ShuffleboardLayout encoderContainer = shuffTab
+      .getLayout("Encoders", BuiltInLayouts.kList)
+      .withSize(1, 2)
+      .withPosition(2, 0);
+
+    encoderContainer.add("Lower Arm Encoder", lowerArmEncoder);
+    encoderContainer.add("Upper Arm Encoder", upperArmEncoder);
   }
 
   public double clamp(double value, double min, double max){
